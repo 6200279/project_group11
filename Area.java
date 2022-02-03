63fb34141a805ba7c0b15ef31b993aaa4674cc39
Benjum9 <72268439+Benjum9@users.noreply.github.com>
2022-02-03 21:45:51 +0100

    test gui

--- Area.java
+++ Area.java
@@ -14,6 +14,10 @@ public class Area {
     protected int rightBoundary;
     protected int topBoundary;
     protected int bottomBoundary;
+    private int  x1 ;
+    private int x2 ;
+    private int y1 ;
+    private  int y2 ;
     
     public Area(){
         leftBoundary=0;
@@ -23,6 +27,10 @@ public class Area {
     }
     
     public Area(int x1, int y1, int x2, int y2){
+        this.x1 = x1 ;
+        this.x2 = x2 ;
+        this.y1 = y1 ;
+        this.y2 = y2 ;
         leftBoundary=Math.min(x1,x2);
         rightBoundary=Math.max(x1,x2);
         topBoundary=Math.max(y1,y2);
@@ -43,4 +51,15 @@ public class Area {
     public boolean isHit(double x,double y,double radius){
         return false;
     }
+
+    public int getX1(){return x1 ; }
+
+    public int getX2() {
+        return x2;
+    }
+    public int getY1(){return y1;}
+
+    public int getY2() {return y2;}
+
+
 }
---
+++ MainFrameTest.java
@@ -0,0 +1,31 @@
+package nl.maastrichtuniversity.dke.gamecontrollersample;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrameTest {
+
+    private Scenario scenario = new Scenario("testmap.txt") ;
+
+    public static void main(String[] args) {
+        buildframe();
+
+
+    }
+
+    public static void buildframe(){
+        JFrame frame = new JFrame();
+        frame.setSize(120*7, 80*7);
+        frame.setTitle("Test");
+        AreaComponent ac = new AreaComponent() ;
+        frame.add(ac) ;
+
+
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
+
+}
