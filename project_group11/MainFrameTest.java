f6e844ea15c3c6b99a7d5aaef8327327a5a3d96e
[Max Goldman] <g.ciattaglia@studen.maastrichtuniversity.nl>
2022-02-12 20:48:28 +0100

    starting menu and music button

--- project_group11/MainFrameTest.java
+++ project_group11/MainFrameTest.java
@@ -1,8 +1,11 @@
 package project_group11;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Random;
+
 
 public class MainFrameTest {
     private static final int DEFAULT_HEIGHT = 80;
@@ -10,6 +13,8 @@ public class MainFrameTest {
     private static final int DEFAULT_WIDTH = 120;
 
     private Scenario scenario = new Scenario("testmap.txt") ;
+
+    private Image backG; // background
 
     public static void main(String[] args) {
         int height = DEFAULT_HEIGHT;
@@ -35,20 +40,63 @@ public class MainFrameTest {
         List<Point> Lake = terraingenerator.Map.Terrain_mapper("LAKE");
         List<Point> plains = terraingenerator.Map.Terrain_mapper("PLAINS");
 
-
         JFrame frame = new JFrame();
+        JFrame frame2 = new JFrame();
         frame.setSize(120*10, 80*10);
-        frame.setTitle("Test");
-        //AreaComponent ac = new AreaComponent() ;
+        final ImageIcon image = new ImageIcon(new ImageIcon("assets/img.jpeg").getImage().getScaledInstance(120*10, 80*10, Image.SCALE_DEFAULT));
+        frame.setLayout(new BorderLayout());
 
-        frame.getContentPane().add(terrain);
+        // Start Button
+        JLabel bg= new JLabel(image);
+        frame.add(bg);
+        bg.setLayout(null);
+        int fixedButtonWidth = 250;
+        int fixedButtonHeight = 20;
+        final ImageIcon image2= new ImageIcon(new ImageIcon("assets/img2.jpeg").getImage().getScaledInstance(fixedButtonWidth,fixedButtonHeight,Image.SCALE_DEFAULT));
+        JButton b= new JButton("Start the game",image2);
+        b.setHorizontalTextPosition(SwingConstants.CENTER);
+        b.setSize(150, 22);
+        b.setForeground(Color.WHITE);
+        b.setFont(new Font("Dialog", Font.PLAIN,15));
+        b.setLocation(frame.getWidth()/2-40, frame.getHeight()/2+170);
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                frame2.setVisible(true);
+            }
+        });
+        bg.add(b);
 
-        //frame.getContentPane().add(ac) ;
+        // Music
+        JCheckBox bMusic= new JCheckBox("Music");
+        bMusic.setBounds(140,150,80,20);
+        bMusic.setFont(new Font("Dialog", Font.PLAIN,17));
+        bMusic.setLocation(10, 10);
+        bMusic.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (bMusic.isSelected())
+                {
+                    System.out.println("Music is supposed to play");
+                }
+            }
+        });
+        bg.add(bMusic);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Main Menu");
         frame.setVisible(true);
 
 
+        frame2.setSize(120*10, 80*10);
+        frame2.setTitle("Test");
+        //AreaComponent ac = new AreaComponent() ;
+        frame2.getContentPane().add(terrain);
+        //frame2.getContentPane().add(ac) ;
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
+
 
 }
