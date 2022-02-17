e9727f4a8fab8f1b67d0f7c47dd6e67efe25bd78
6200279 <45390835+6200279@users.noreply.github.com>
2022-02-17 13:11:32 +0100

    merged branches

--- project_group11/project_group11/MainFrameTest.java
+++ project_group11/project_group11/MainFrameTest.java
@@ -3,13 +3,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.List;
 import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.util.Random;
+
 
 public class MainFrameTest {
 
 
 
     private static Scenario scenario = new Scenario("testmap.txt") ;
-
+    private Image backG; // background
     public static void main(String[] args) {
         int height = scenario.mapHeight;
         int width = scenario.mapWidth;
@@ -41,22 +48,91 @@ public class MainFrameTest {
         System.out.println("Amount of plains points:"+plains.size());
 
 
+
         JFrame frame = new JFrame();
-        frame.setSize(120*scale, 80*scale);
-        frame.setTitle("Test");
-        //AreaComponent ac = new AreaComponent() ;
+        JFrame frame2 = new JFrame();
+        frame.setSize(120*10, 80*10);
+        final ImageIcon image = new ImageIcon(new ImageIcon("assets/img.jpeg").getImage().getScaledInstance(120*10, 80*10, Image.SCALE_DEFAULT));
+        frame.setLayout(new BorderLayout());
 
-        frame.getContentPane().add(terrain);
+        // Start Button
+        JLabel bg= new JLabel(image);
+        frame.add(bg);
+        bg.setLayout(null);
+        int fixedButtonWidth = 250;
+        int fixedButtonHeight = 20;
+        final ImageIcon image2= new ImageIcon(new ImageIcon("assets/img2.jpeg").getImage().getScaledInstance(fixedButtonWidth,fixedButtonHeight,Image.SCALE_DEFAULT));
+        JButton b= new JButton("Start the game",image2);
+        b.setHorizontalTextPosition(SwingConstants.CENTER);
+        b.setSize(150, 22);
+        b.setForeground(Color.WHITE);
+        b.setFont(new Font("Dialog", Font.PLAIN,15));
+        b.setLocation(frame.getWidth()/2, frame.getHeight()/2+170);
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                frame2.setVisible(true);
+            }
+        });
+        bg.add(b);
 
-        //frame.getContentPane().add(ac) ;
+        JComboBox<String>cb = new JComboBox<String>() ;
+        cb.setVisible(true);
+        cb.setSize(150,22);
+        cb.setLocation(frame.getWidth()/2-200,frame.getHeight()/2+170);
+        bg.add(cb) ;
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Music
+        JCheckBox bMusic= new JCheckBox("Music");
+        bMusic.setBounds(140,150,80,20);
+        bMusic.setFont(new Font("Dialog", Font.PLAIN,17));
+        bMusic.setLocation(10, 10);
+        bMusic.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (bMusic.isSelected())
+                {
+                    System.out.println("Music is supposed to play");
+                }
+            }
+        });
+        bg.add(bMusic);
+
+        frame.setTitle("Main Menu");
         frame.setVisible(true);
+
+
+        frame2.setSize(120*scale, 80*scale);
+        frame2.setTitle("Test");
+        //AreaComponent ac = new AreaComponent() ;
+        frame2.getContentPane().add(BorderLayout.CENTER,terrain);
+
+        JPanel panelWest = new JPanel() ;
+        JPanel panelEast = new JPanel() ;
+
+        panelEast.setBackground(new Color(199, 133, 133));
+        panelWest.setBackground(new Color(199,133,133));
+
+        JButton button = new JButton("TEST") ;
+        JButton button1 = new JButton("TEST") ;
+
+        panelEast.add(button1) ;
+        panelWest.add(button) ;
+
+
+        frame2.add(BorderLayout.EAST,panelEast) ;
+        frame2.add(BorderLayout.WEST,panelWest) ;
+        //frame2.getContentPane().add(ac) ;
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
 
         AgentViewPoints myAgent = new AgentViewPoints();
         Point myPoint = new Point(25,25);
         //myAgent.rotate(myPoint, 10);
-        myAgent.ringOfPoints();
+
         //myAgent.farthest();
         //myAgent.viewPoints();
 
@@ -64,4 +140,3 @@ public class MainFrameTest {
 
     }
 
-}
