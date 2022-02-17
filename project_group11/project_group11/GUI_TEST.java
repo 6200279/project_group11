d3aab90ce286bf750c36686e6d8975e6f39e8e33
6200279 <45390835+6200279@users.noreply.github.com>
2022-02-17 13:45:10 +0100

    merged branches

--- project_group11/project_group11/GUI_TEST.java
+++ project_group11/project_group11/GUI_TEST.java
@@ -1,33 +1,37 @@
 package project_group11;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Random;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+public class GUI_TEST {
+    private static final int DEFAULT_HEIGHT = 80;
 
+    private static final int DEFAULT_WIDTH = 120;
 
-public class MainFrameTest {
+    private Scenario scenario = new Scenario("testmap.txt") ;
 
-
-
-    private static Scenario scenario = new Scenario("testmap.txt") ;
     private Image backG; // background
+
     public static void main(String[] args) {
-        int height = scenario.mapHeight;
-        int width = scenario.mapWidth;
-        int scale = 10; // scale the map size
+        int height = DEFAULT_HEIGHT;
+        int width = DEFAULT_WIDTH;
         //long seed = System.currentTimeMillis();
         long seed =13515420;
-
+        if (args.length == 3)
+        {
+            height = Integer.parseInt(args[0]);
+            width = Integer.parseInt(args[1]);
+            seed = Long.parseLong(args[2]);
+        }
 
         Random rand = new Random(seed);
         double z = rand.nextDouble();
-        // BIOME TYPES include, GREEK, SAHARA
-        String biome = "GREEK";
-        terraingenerator.Map terrain = new terraingenerator.Map(height, width, z,scale, biome);
+
+        terraingenerator.Map terrain = new terraingenerator.Map(height, width, z,10,"GREEK");
         List<Point> snow = terraingenerator.Map.Terrain_mapper("SNOW");
         List<Point> forest = terraingenerator.Map.Terrain_mapper("FOREST");
         List<Point> hills = terraingenerator.Map.Terrain_mapper("HILLS");
@@ -36,20 +40,9 @@ public class MainFrameTest {
         List<Point> Lake = terraingenerator.Map.Terrain_mapper("LAKE");
         List<Point> plains = terraingenerator.Map.Terrain_mapper("PLAINS");
 
-        System.out.println("BIOME TYPE :"+ biome);
-        System.out.println("Amount of snowpoints:"+snow.size());
-        System.out.println("Amount of forest points:"+forest.size());
-        System.out.println("Amount of hills points:"+hills.size());
-        System.out.println("Amount of Mountains points:"+Mountains.size());
-        System.out.println("Amount of Desert points:"+Desert.size());
-        System.out.println("Amount of lake points:"+Lake.size());
-        System.out.println("Amount of plains points:"+plains.size());
-
-
-
         JFrame frame = new JFrame();
         JFrame frame2 = new JFrame();
-        frame.setSize(120*scale, 80*scale);
+        frame.setSize(120*10, 80*10);
         final ImageIcon image = new ImageIcon(new ImageIcon("assets/img.jpeg").getImage().getScaledInstance(120*10, 80*10, Image.SCALE_DEFAULT));
         frame.setLayout(new BorderLayout());
 
@@ -65,12 +58,12 @@ public class MainFrameTest {
         b.setSize(150, 22);
         b.setForeground(Color.WHITE);
         b.setFont(new Font("Dialog", Font.PLAIN,15));
-        b.setLocation(frame.getWidth()/2, frame.getHeight()/2+170);
+        b.setLocation(frame.getWidth()/2-40, frame.getHeight()/2+170);
         b.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.setVisible(true);
-                frame2.setVisible(false);
+                frame.setVisible(false);
+                frame2.setVisible(true);
             }
         });
         bg.add(b);
@@ -95,26 +88,17 @@ public class MainFrameTest {
 
         frame.setTitle("Main Menu");
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame2.setSize(120*scale, 80*scale);
-        frame2.setTitle("Testing");
+
+        frame2.setSize(120*10, 80*10);
+        frame2.setTitle("Test");
         //AreaComponent ac = new AreaComponent() ;
-        frame2.getContentPane().add(BorderLayout.CENTER,terrain);
-
+        frame2.getContentPane().add(terrain);
+        //frame2.getContentPane().add(ac) ;
         frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
     }
 
-        //AgentViewPoints myAgent = new AgentViewPoints();
-        //Point myPoint = new Point(25,25);
-        //myAgent.rotate(myPoint, 10);
 
-        //myAgent.farthest();
-        //myAgent.viewPoints();
-
-
-
-    }
-
+}
